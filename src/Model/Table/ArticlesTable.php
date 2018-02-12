91f47be1cfde47dc6ab926f8e75bfda5c03f652a
MAV <matthias.vincent@manessens.com>
2018-02-12 17:09:10 +0100

    md5 ref

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -3,6 +3,7 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Utility\Text;
 
 class ArticlesTable extends Table
 {
@@ -10,4 +11,15 @@ class ArticlesTable extends Table
     {
         $this->addBehavior('Timestamp');
     }
+
+    public function beforeSave($event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->ref) {
+            $ref = md5($entity->title);
+            // On ne garde que le nombre de caractère correspondant à la longueur
+            // maximum définie dans notre schéma
+            $entity->ref = $ref;
+        }
+    }
+
 }
