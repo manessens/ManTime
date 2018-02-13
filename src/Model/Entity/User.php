74cbf1d36702ad2c1cd17b788ad1b4d53007c372
root <root@srv-sles-da05.manessens.loc>
2018-02-13 11:23:20 +0100

    test add users

---
+++ src/Model/Entity/User.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * User Entity
+ *
+ * @property int $idu
+ * @property string $prenom
+ * @property string $nom
+ * @property string $email
+ * @property string $mdp
+ * @property int $actif
+ * @property int $prem_connect
+ *
+ * @property \App\Model\Entity\Article[] $articles
+ */
+class User extends Entity
+{
+
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array
+     */
+    protected $_accessible = [
+        'prenom' => true,
+        'nom' => true,
+        'email' => true,
+        'mdp' => true,
+        'actif' => true,
+        'prem_connect' => true,
+        'articles' => true
+    ];
+}
---
+++ tests/Fixture/UsersFixture.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * UsersFixture
+ *
+ */
+class UsersFixture extends TestFixture
+{
+
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'idu' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+        'prenom' => ['type' => 'string', 'length' => 50, 'null' => true, 'default' => null, 'collate' => 'utf8_unicode_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'nom' => ['type' => 'string', 'length' => 50, 'null' => true, 'default' => '0', 'collate' => 'utf8_unicode_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'email' => ['type' => 'string', 'length' => 50, 'null' => false, 'default' => '0', 'collate' => 'utf8_unicode_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'mdp' => ['type' => 'string', 'length' => 32, 'null' => false, 'default' => '0', 'collate' => 'utf8_unicode_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'actif' => ['type' => 'integer', 'length' => 1, 'unsigned' => true, 'null' => true, 'default' => '0', 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'prem_connect' => ['type' => 'integer', 'length' => 1, 'unsigned' => true, 'null' => true, 'default' => '1', 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['idu'], 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collation' => 'utf8_unicode_ci'
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [
+        [
+            'idu' => 1,
+            'prenom' => 'Lorem ipsum dolor sit amet',
+            'nom' => 'Lorem ipsum dolor sit amet',
+            'email' => 'Lorem ipsum dolor sit amet',
+            'mdp' => 'Lorem ipsum dolor sit amet',
+            'actif' => 1,
+            'prem_connect' => 1
+        ],
+    ];
+}
