452ac9de9d6f4f32a277f01d508ddc14425e47a0
MAV <matthias.vincent@manessens.com>
2018-02-14 11:24:51 +0100

    add hash mdp

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Model\Entity;
 
+use Cake\Auth\DefaultPasswordHasher;
 use Cake\ORM\Entity;
 
 /**
@@ -37,4 +38,14 @@ class User extends Entity
         'prem_connect' => true,
         'articles' => true
     ];
+
+    protected function _setPassword($value)
+    {
+        if (strlen($value)) {
+            $hasher = new DefaultPasswordHasher();
+
+            return $hasher->hash($value);
+        }
+    }
+
 }
