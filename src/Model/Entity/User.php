f2be693e0ebcc09ee78e90e21e94613f0fccdecb
MAV <matthias.vincent@manessens.com>
2018-02-14 11:37:01 +0100

    correct hash pssw

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -43,7 +43,7 @@ class User extends Entity
     {
         if (strlen($value)) {
             $hasher = new DefaultPasswordHasher();
-
+            
             return $hasher->hash($value);
         }
     }
