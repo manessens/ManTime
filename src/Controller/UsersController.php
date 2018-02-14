5d8215785a3f35cd6931756f24620eb041b7a5e4
MAV <matthias.vincent@manessens.com>
2018-02-14 16:39:13 +0100

    add debug

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -140,6 +140,7 @@ class UsersController extends AppController
         if (in_array($action, ['index', 'view']) ) {
             return true;
         }
+        pr($user['admin']);exit;
 
         if (in_array($action, ['add', 'edit']) && $user['admin'] === 1 ) {
             return true;
