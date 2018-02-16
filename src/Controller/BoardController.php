740300cbac0bea5c1e7164770d1199e4ac77a150
MAV <matthias.vincent@manessens.com>
2018-02-16 11:33:08 +0100

    fixed auth

--- src/Controller/BoardController.php
+++ src/Controller/BoardController.php
@@ -11,4 +11,20 @@ class BoardController extends AppController
         $this->set(compact('user'));
     }
 
+    public function isAuthorized($user)
+    {
+        $action = $this->request->getParam('action');
+        // Les actions 'add' et 'tags' sont toujours autorisés pour les utilisateur
+        // authentifiés sur l'application
+        if ($user['prem_connect'] === 1) {
+            return false;
+        }
+
+        if (in_array($action, ['index']) ) {
+            return true;
+        }
+
+        return false;
+    }
+
 }
