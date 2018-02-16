947b0d9b0add63fc390dc2bbb4b4e1aa865b9884
MAV <matthias.vincent@manessens.com>
2018-02-16 14:02:22 +0100

    Update AppController.php

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -73,7 +73,7 @@ class AppController extends Controller
 
         // Permet à l'action "display" de notre PagesController de continuer
         // à fonctionner. Autorise également les actions "read-only".
-        $this->Auth->allow(['login']);
+        $this->Auth->allow(['display','login']);
     }
 
     public function isAuthorized($user)
