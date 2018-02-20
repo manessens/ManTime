ba64218ea05658ef0248858f3fa57d84abf43997
MAV <matthias.vincent@manessens.com>
2018-02-20 17:10:25 +0100

    Update MatriceController.php

--- src/Controller/MatriceController.php
+++ src/Controller/MatriceController.php
@@ -59,16 +59,16 @@ class MatriceController extends AppController
             $lignMats = $matrice->lign_mat;
             $matrice->lign_mat = [];
             if ($this->Matrice->save($matrice)) {
+                $matrice->lign_mat = $lignMats;
+                $matrice->lign_mat[0]->idm = $matrice->idm;
+                $matrice->lign_mat[0]->id_profil = 1;
+                $matrice->lign_mat[1]->idm = $matrice->idm;
+                $matrice->lign_mat[1]->id_profil = 2;
+                $matrice->lign_mat[2]->idm = $matrice->idm;
+                $matrice->lign_mat[2]->id_profil = 3;
+                $matrice->lign_mat[3]->idm = $matrice->idm;
+                $matrice->lign_mat[3]->id_profil = 4;
                 if ($this->Matrice->save($matrice, ['associated' => ['LignMat']])) {
-                    $matrice->lign_mat = $lignMats;
-                    $matrice->lign_mat[0]->idm = $matrice->idm;
-                    $matrice->lign_mat[0]->id_profil = 1;
-                    $matrice->lign_mat[1]->idm = $matrice->idm;
-                    $matrice->lign_mat[1]->id_profil = 2;
-                    $matrice->lign_mat[2]->idm = $matrice->idm;
-                    $matrice->lign_mat[2]->id_profil = 3;
-                    $matrice->lign_mat[3]->idm = $matrice->idm;
-                    $matrice->lign_mat[3]->id_profil = 4;
                     $this->Flash->success(__('La matrice a été créée.'));
 
                     return $this->redirect(['action' => 'index']);
