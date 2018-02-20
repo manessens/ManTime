934149d57f7cc3c79dfbffb13a03c5143982d3c5
MAV <matthias.vincent@manessens.com>
2018-02-20 17:04:41 +0100

    other debug

--- src/Controller/MatriceController.php
+++ src/Controller/MatriceController.php
@@ -60,6 +60,7 @@ class MatriceController extends AppController
             $matrice->lign_mat[1]->id_profil = 2;
             $matrice->lign_mat[2]->id_profil = 3;
             $matrice->lign_mat[3]->id_profil = 4;
+            pr($matrice);exit;
             if ($this->Matrice->save($matrice, ['associated' => ['LignMat']])) {
                 $this->Flash->success(__('La matrice a été créée.'));
 
