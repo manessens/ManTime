fb76de59a11791564fb3c7a86b053c2f93092c4a
MAV <matthias.vincent@manessens.com>
2018-02-22 10:55:25 +0100

    test

--- webroot/js/ManTime/man.projet.js
+++ webroot/js/ManTime/man.projet.js
@@ -1,6 +1,6 @@
-$(function() {
-    $( ".datepicker" ).each(function() {
-        $( this ).attr('value', moment($( this ).attr('value'), "DD/MM/YYYY hh:mm").format("YYYY-MM-DD"));
-        $( this ).attr('type', 'date');
-    });
-});
+// $(function() {
+//     $( ".datepicker" ).each(function() {
+//         $( this ).attr('value', moment($( this ).attr('value'), "DD/MM/YYYY hh:mm").format("YYYY-MM-DD"));
+//         $( this ).attr('type', 'date');
+//     });
+// });
