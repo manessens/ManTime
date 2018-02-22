034bd1fd6c4adb4559d904277cae0cd95553194f
MAV <matthias.vincent@manessens.com>
2018-02-22 10:57:54 +0100

    Update man.projet.js

--- webroot/js/ManTime/man.projet.js
+++ webroot/js/ManTime/man.projet.js
@@ -1,6 +1,6 @@
 $(function() {
     $( ".datepicker" ).each(function() {
-        if ($( this ).attr('value').length() > 10) {
+        if ($( this ).attr('value').length > 10) {
             $( this ).attr('value', moment($( this ).attr('value'), "DD/MM/YYYY hh:mm").format("YYYY-MM-DD"));
         }
         $( this ).attr('type', 'date');
