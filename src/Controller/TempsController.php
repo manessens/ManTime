ae642ddc389675525d33e3907bc67ba91b103db6
MAV <matthias.vincent@manessens.com>
2018-02-26 17:03:28 +0100

    Update TempsController.php

--- src/Controller/TempsController.php
+++ src/Controller/TempsController.php
@@ -41,7 +41,7 @@ class TempsController extends AppController
 
         // $lundi->i18nFormat('dd/MM');
         $dimanche = clone $lundi;
-        $dimanche->day += 7;
+        $dimanche->modfy('+7 days');
         pr($lundi);
         pr($dimanche);exit;
         // date("W", strtotime($dimanche->i18nFormat('YYYY-MM-/dd')));
