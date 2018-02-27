5dcd2125a04260ea87e7bb2346b1b50729050fb3
AngeHell <AngeHell@ANGEHELL.local>
2018-02-27 16:25:43 +0100

    Update man.time.js

--- webroot/js/ManTime/man.time.js
+++ webroot/js/ManTime/man.time.js
@@ -10,7 +10,11 @@ $( ".client" ).change(function (e) {
     $( select ).find('option').each(function() {
         if ( $.inArray($( this ).val(), optionProjects[idc]) != -1 ) {
             $( this ).show();
-            $( select ).val(optionProjects[idc][0]);
+            if ($( select ).find('option[selected=selected]:visible').length ){
+                $( select ).val($( select ).find('option[selected=selected]:visible').val());
+            }else{
+                $( select ).val(optionProjects[idp][0]);
+            }
         }else{
             $( this ).hide();
         }
@@ -19,7 +23,11 @@ $( ".client" ).change(function (e) {
     $( select2 ).find('option').each(function() {
         if ( $.inArray($( this ).val(), optionProfils[idc]) != -1 ) {
             $( this ).show();
-            $( select2 ).val(optionProfils[idc][0]);
+            if ($( select ).find('option[selected=selected]:visible').length ){
+                $( select ).val($( select ).find('option[selected=selected]:visible').val());
+            }else{
+                $( select ).val(optionProfils[idp][0]);
+            }
         }else{
             $( this ).hide();
         }
@@ -28,12 +36,16 @@ $( ".client" ).change(function (e) {
 
 $( ".project" ).change(function (e) {
     var val = $(this).val();
-    var idp = val.split('.')[0];
+    var idp = val.split('.')[1];
     var select = $( this ).parent().parent().find('td.cel_activit').children();
     $( select ).find('option').each(function() {
         if ( $.inArray($( this ).val(), optionActivits[idp]) != -1 ) {
             $( this ).show();
-            $( select ).val(optionActivits[idp][0]);
+            if ($( select ).find('option[selected=selected]:visible').length ){
+                $( select ).val($( select ).find('option[selected=selected]:visible').val());
+            }else{
+                $( select ).val(optionActivits[idp][0]);
+            }
         }else{
             $( this ).hide();
         }
