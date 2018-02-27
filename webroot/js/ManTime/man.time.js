1b3f256e64639ad2f3f8a49b13996923faa278b8
AngeHell <AngeHell@ANGEHELL.local>
2018-02-27 16:51:43 +0100

    add debug in js

--- webroot/js/ManTime/man.time.js
+++ webroot/js/ManTime/man.time.js
@@ -15,8 +15,10 @@ $( ".client" ).change(function (e) {
     });
     if ($( select ).find('option[selected=selected]:visible').length ){
         $( select ).val($( select ).find('option[selected=selected]:visible').val());
+        console.log($( select ).find('option[selected=selected]:visible').val());
     }else{
         $( select ).val(optionProjects[idc][0]);
+        console.log('prout');
     }
     $( ".project" ).change();
     var select2 = $( this ).parent().parent().find('td.cel_profil').children();
