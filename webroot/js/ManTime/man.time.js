c59812710e668cdd298fe70a79acc3b3cecd1519
AngeHell <AngeHell@ANGEHELL.local>
2018-02-27 16:10:22 +0100

    update js on select

--- webroot/js/ManTime/man.time.js
+++ webroot/js/ManTime/man.time.js
@@ -1,18 +1,7 @@
 $(function() {
-    // var id = $('#semainier tbody tr').length-1;
-    // var node = $('#'+(id-1)).clone();
-    // node = node.html().replace(id-1, id);
-    // $('#semainier>tbody:last').append(node);
     $( ".client" ).change();
-
+    $( ".project" ).change();
 });
-// $( ".project" ).change(function () {
-//     var val = $(this).val();
-//     var idp = val.split('.')[0];
-//     // $(this).parent().next().children().val(idp);
-//     $(this).parent().next().children().find('option[value!='+idp+']').hide();
-//     $(this).parent().next().children().find('option[value='+idp+']').show();
-// }).change();
 
 $( ".client" ).change(function (e) {
     var val = $(this).val();
@@ -26,16 +15,25 @@ $( ".client" ).change(function (e) {
             $( this ).hide();
         }
     });
-})
-
-$( ".client" ).change(function (e) {
-    var val = $(this).val();
-    var idc = val;
-    var select = $( ".client" ).parent().parent().find('td.cel_profil').children();
-    $( select ).find('option').each(function() {
+    var select2 = $( this ).parent().parent().find('td.cel_profil').children();
+    $( select2 ).find('option').each(function() {
         if ( $.inArray($( this ).val(), optionProfils[idc]) != -1 ) {
             $( this ).show();
-            $( select ).val(optionProfils[idc][0]);
+            $( select2 ).val(optionProfils[idc][0]);
+        }else{
+            $( this ).hide();
+        }
+    });
+})
+
+$( ".project" ).change(function (e) {
+    var val = $(this).val();
+    var idp = val.split('.')[0];
+    var select = $( this ).parent().parent().find('td.cel_activit').children();
+    $( select ).find('option').each(function() {
+        if ( $.inArray($( this ).val(), optionActivits[idp]) != -1 ) {
+            $( this ).show();
+            $( select ).val(optionActivits[idp][0]);
         }else{
             $( this ).hide();
         }
