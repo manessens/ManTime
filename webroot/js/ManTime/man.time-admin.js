34caccb4c2574262c9e4f0e6dc9caa66e607fe3d
MAV <matthias.vincent@manessens.com>
2018-03-02 13:59:42 +0100

    Update man.time-admin.js

--- webroot/js/ManTime/man.time-admin.js
+++ webroot/js/ManTime/man.time-admin.js
@@ -21,8 +21,9 @@ $( "form" ).on('submit',function (e){
                     alertVerouillage = true;
                 }else{
                     alertVerouillage = false;
-                    $( "form" ).submit();
                 }
+                $( "form" ).submit();
+                alertVerouillage = $('#validat').prop('checked');
             },
             Center: true,
             AllowClickAway: false
@@ -32,7 +33,6 @@ $( "form" ).on('submit',function (e){
     if (alertVerouillage) {
         e.preventDefault();
     }
-    $( "#validat" ).click();
 });
 
 $( "#validat" ).click(function(){
