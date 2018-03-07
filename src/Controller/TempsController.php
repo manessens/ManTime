9fc342054a3fccfc17b14c5396755ba47d399461
MAV <matthias.vincent@manessens.com>
2018-03-07 11:12:12 +0100

    Update TempsController.php

--- src/Controller/TempsController.php
+++ src/Controller/TempsController.php
@@ -686,19 +686,29 @@ class TempsController extends AppController
                         foreach ($arrProfil as $profil => $arrActiv) {
                             foreach ($arrActiv as $activit => $arrDate) {
                                 $buffer = ['client'=>$client, 'projet'=>$projet, 'user'=>$user, 'profil'=>$profil,'activit'=>$activit];
+                                $timebuffer = [];
+                                $timebufferMonth = $arrayMonth;
+                                $UobufferMonth = $arrayMonth;
+                                $CabufferMonth = $arrayMonth;
                                 foreach ($arrDate as $date => $arrTime) {
-                                    $timebuffer = [];
-                                    $timebufferMonth = $arrayMonth;
                                     foreach ($arrTime as $type => $time) {
                                         $monthKey = explode('-',$date)[1] -1;
-                                        if ($type === 'CA') {
-                                            $timebufferMonth[$monthKey] += ($time*$arrayClientPrice[$client]);
-                                        }else{
-                                            $timebufferMonth[$monthKey] += $time;
+                                        switch ($type) {
+                                            case 'UO':
+                                                $UobufferMonth[$monthKey] = $time;
+                                                break;
+                                            case 'CA':
+                                                $CabufferMonth[$monthKey] = ($time*$arrayClientPrice[$client]);
+                                                break;
+                                            default:
+                                                $timebufferMonth[$monthKey] = $time;
+                                                break;
                                         }
                                     }
-                                    $timebuffer = array_merge($timebuffer, $timebufferMonth);
                                 }
+                                $timebufferMonth = array_merge($timebufferMonth, $UobufferMonth);
+                                $timebufferMonth = array_merge($timebufferMonth, $CabufferMonth);
+                                $timebuffer = array_merge($timebuffer, $timebufferMonth);
                                 $dataLine[] = array_merge($buffer, $timebuffer);
                             }
                         }
