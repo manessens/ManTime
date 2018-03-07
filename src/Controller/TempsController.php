9af5beb5d5d1554f1854efc51c2cb47e912bcc84
MAV <matthias.vincent@manessens.com>
2018-03-07 10:57:37 +0100

    remove debug Final version 3.0

--- src/Controller/TempsController.php
+++ src/Controller/TempsController.php
@@ -666,10 +666,6 @@ class TempsController extends AppController
                 $timeUO =  $arrayMatrice[$keyClient][$keyProfil]['j'];
             }else{
                 $timeUO = round($time->time * 8, 1) * $arrayMatrice[$keyClient][$keyProfil]['h'];
-                pr($keyProfil);
-                pr($arrayMatrice[$keyClient][$keyProfil]['h']);
-                pr($timeUO);
-                pr('--------------');
             }
             $data[$keyClient][$keyProject][$keyUser][$keyProfil][$keyActivit][$keyDate]['JH']+=$time->time;
             $data[$keyClient][$keyProject][$keyUser][$keyProfil][$keyActivit][$keyDate]['UO']+=$timeUO;
@@ -678,7 +674,6 @@ class TempsController extends AppController
             ksort($data[$keyClient]);
             ksort($data[$keyClient][$keyProject]);
         }
-        exit;
         ksort($data);
         $dataLine=array();
         if ($isFitnet) {
