cb47197761969fd7118e9d1d3ac47a55214761d5
MAV <matthias.vincent@manessens.com>
2018-03-07 12:03:49 +0100

    Update TempsController.php

--- src/Controller/TempsController.php
+++ src/Controller/TempsController.php
@@ -454,7 +454,7 @@ class TempsController extends AppController
         $arrayRetour = array('projets'=>[], 'clients'=>[], 'profiles'=>[], 'activities'=>[]);
         $particpations = $participantTable->find('all')
             ->where(['idu =' => $idu])
-            ->andWhere(['date_debut <' => $dimanche->i18nFormat('YYYY-MM-dd')])
+            ->andWhere(['date_debut <' => $dimanche->i18nFormat([\IntlDateFormatter::FULL, \IntlDateFormatter::SHORT])])
             ->andWhere(['date_fin >=' => $lundi->i18nFormat('YYYY-MM-dd')])
             ->contain(['Projet' => ['Client'=>['Matrice'=>['LignMat'=>['Profil']]]]]);//->all();
                     pr($dimanche);
