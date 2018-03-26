d63b8302f6e2e8b0b7c6b29df0dfe5d2b344c62b
MAV <matthias.vincent@manessens.com>
2018-03-26 16:15:25 +0200

    Update TempsController.php

--- src/Controller/TempsController.php
+++ src/Controller/TempsController.php
@@ -158,8 +158,7 @@ class TempsController extends AppController
             }
             if ($verif) {
                 $this->Flash->success(__('La semaine à été sauvegardé.'));
-
-                // return $this->redirect(['controller'=>'Board', 'action' => 'index']);
+                return $this->redirect(['action' => 'index']);
             }else{
                 $this->Flash->error(__('Une erreur est survenue, veuilez contrôler votre saisie avant de réessayer.'));
             }
