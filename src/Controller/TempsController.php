b202c5711971990fd859a7858a17ed5088d9d735
MAV <matthias.vincent@manessens.com>
2018-04-06 15:58:32 +0200

    add function for "holidays"

--- src/Controller/TempsController.php
+++ src/Controller/TempsController.php
@@ -447,8 +447,7 @@ class TempsController extends AppController
         return $week;
     }
 
-    private function getDay($date, $lundi)
-    {
+    private function getDay($date, $lundi) {
         $mardi = clone $lundi;
         $mardi->modify('+1 days');
         $mercredi = clone $lundi;
@@ -482,6 +481,36 @@ class TempsController extends AppController
         }else {
             return 'Di';
         }
+    }
+
+    private static function getHolidays($year = null){
+        if ($year === null){
+                $year = intval(strftime('%Y'));
+        }
+
+        $easterDate = easter_date($year);
+        $easterDay = date('j', $easterDate);
+        $easterMonth = date('n', $easterDate);
+        $easterYear = date('Y', $easterDate);
+
+        $holidays = array(
+                // Jours feries fixes
+                mktime(0, 0, 0, 1, 1, $year),// 1er janvier
+                mktime(0, 0, 0, 5, 1, $year),// Fete du travail
+                mktime(0, 0, 0, 5, 8, $year),// Victoire des allies
+                mktime(0, 0, 0, 7, 14, $year),// Fete nationale
+                mktime(0, 0, 0, 8, 15, $year),// Assomption
+                mktime(0, 0, 0, 11, 1, $year),// Toussaint
+                mktime(0, 0, 0, 11, 11, $year),// Armistice
+                mktime(0, 0, 0, 12, 25, $year),// Noel
+
+                // Jour feries qui dependent de paques
+                mktime(0, 0, 0, $easterMonth, $easterDay + 1, $easterYear),// Lundi de paques
+                mktime(0, 0, 0, $easterMonth, $easterDay + 39, $easterYear),// Ascension
+                mktime(0, 0, 0, $easterMonth, $easterDay + 50, $easterYear), // Pentecote
+        );
+        sort($holidays);
+        return $holidays;
     }
 
     private function getProjects($idu, $lundi, $dimanche)
