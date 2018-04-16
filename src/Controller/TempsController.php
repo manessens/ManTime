267e33b9f6ee3ce186adf6afa4eb7cdfacd50d5a
MVIN <matthias.vincent@manessens.com>
2018-04-16 15:48:31 +0200

    Update TempsController.php

--- src/Controller/TempsController.php
+++ src/Controller/TempsController.php
@@ -816,6 +816,7 @@ class TempsController extends AppController
 
             $data[$keyClient][$keyProject][$keyUser][$keyProfil][$keyActivit][$nLine][$keyDate]['JH']+=$time->time;
             //majoration si samedi : *1.5 dimanche : *2 jour férié : *2
+            pr($this->getIncreaseDay($dateTime)));
             $data[$keyClient][$keyProject][$keyUser][$keyProfil][$keyActivit][$nLine][$keyDate]['UO']+=($timeUO * $this->getIncreaseDay($dateTime));
             $data[$keyClient][$keyProject][$keyUser][$keyProfil][$keyActivit][$nLine][$keyDate]['CA']+=$timeUO*$time->prix;
             $data[$keyClient][$keyProject][$keyUser][$keyProfil][$keyActivit][$nLine]['detail']=$this->convertToIso($time->detail);
@@ -825,7 +826,7 @@ class TempsController extends AppController
             ksort($data[$keyClient][$keyProject][$keyUser][$keyProfil][$keyActivit]);
         }
         ksort($data);
-        $dataLine=array();
+        $dataLine=array();exit;
         foreach ($data as $client => $arrProj) {
             foreach ($arrProj as $projet => $arrUser) {
                 foreach ($arrUser as $user => $arrProfil) {
