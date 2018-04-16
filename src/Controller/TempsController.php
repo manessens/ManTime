0277dd31e7f48f8efd894ffee0a3ad31b3649155
MVIN <matthias.vincent@manessens.com>
2018-04-16 15:24:33 +0200

    Update TempsController.php

--- src/Controller/TempsController.php
+++ src/Controller/TempsController.php
@@ -730,7 +730,7 @@ class TempsController extends AppController
 
     private function getIncreaseDay($day)
     {
-        $dateDay = date('w', time($day));
+        $dateDay = date('w', $day);
         pr($dateDay);
         pr(strstr($dateDay,"Sunday"));exit;
         if (strstr($dateDay,"Sunday")){
