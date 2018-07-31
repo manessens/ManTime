29c2be8d30902268bd48c4d04914e70937848329
MVIN <matthias.vincent@manessens.com>
2018-07-31 17:04:29 +0200

    remove debug and fix success

--- webroot/js/ManTime/man.users.js
+++ webroot/js/ManTime/man.users.js
@@ -30,8 +30,7 @@ function init(){
                 $('#linkModal').find(".modal-footer button#send").hide();
             }
         }).done(function( data ) {
-            console.log(data);
-            if (data.lenght > 0 ) {
+            if ( jQuery.isEmptyObject(data) ) {
                 $('#linker').removeClass('btn-primary');
                 $('#linker').addClass('btn-success');
             }
