bbd68186eb6cea8483af951e7f67863a6a88459f
MVIN <matthias.vincent@manessens.com>
2018-08-06 17:33:24 +0200

    Update man.top.js

--- webroot/js/ManTime/man.top.js
+++ webroot/js/ManTime/man.top.js
@@ -8,7 +8,7 @@ function init(){
 }
 
 function scrollFunction() {
-    if (window.scrollY > 50 && window.scrollY <= (document.documentElement.scrollTop - 150) ) {
+    if (window.scrollY > 200 && window.scrollY <= (document.documentElement.scrollTopMax - 150) ) {
         $(".bottom_fix").show();
     } else {
         $(".bottom_fix").hide();
