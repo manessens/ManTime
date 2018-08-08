9757c40bc9cf6cc35a9d20b6e018cb21deb50702
MVIN <matthias.vincent@manessens.com>
2018-08-08 15:17:25 +0200

    testng

--- src/Shell/FitnetShell.php
+++ src/Shell/FitnetShell.php
@@ -2,7 +2,6 @@
 namespace App\Shell;
 
 use Cake\Console\Shell;
-use Cake\Controller\TempsController;
 
 class FitnetShell extends Shell
 {
@@ -11,19 +10,52 @@ class FitnetShell extends Shell
     {
         parent::initialize();
         $this->loadModel('Users');
+        $this->loadModel('Client');
     }
 
     public function main()
     {
         // $this->out('Hello world.');
         if (empty($this->args[0])) {
-            return $this->abort("Merci de rentrer un nom d'utilisateur.");
+            return $this->abort("Merci de rentrer un email d'utilisateur.");
         }
-        $user = $this->Users->findByEmail($this->args[0])->first();
+        // $user = $this->Users->findByEmail($this->args[0])->first();
         // $this->out(print_r($user, true));
-        $tempsController = new TempsController();
-        $tempsController->constructClasses(); //I needed this in here for more complicated requiring component loads etc in the Controller
-        $tempsController->test();
-        $this->createFile('report.json', json_encode($user));
+
+        // $this->createFile('report.json', json_encode($user));
+        // ***************************************************************
+        $found = [];
+
+        // récupération des id company fitnet
+        $clientTable = TableRegistry::get('Client');
+        $client = $this->Client->findByIdc($id_client, [
+            'contain' => ['Agence']
+        ]);
+
+        $id_fit = $client->agence->id_fit;
+
+        // séparation des id_agence fitnet
+        $ids = explode(';', $id_fit);
+
+        // appel de la requête
+        $result = $this->getFitnetLink("/FitnetManager/rest/projects/".$this->args[0]);
+        // décode du résultat json
+        $vars = json_decode($result, true);
+        // sauvegarde des résultats trouvés
+        $found = array_merge($found, $vars);
+
+        $select2 = ['select' => array(), 'projects' => array()];
+        //remise en forme du tableau
+        foreach ($found as $value) {
+            if ($value['customer'] == $client->id_fit or $client->id_fit == null) {
+                $select2['select'][]=array('id'=>$value['forfaitId'], 'text'=>$value['title']);
+                $select2['projects'][$value['forfaitId']]=$value;
+            }
+        }
+
+        // réencodage pour renvoie au script ajax
+        $json_found = json_encode($select2);
+
+        $this->createFile('report.json', json_encode($json_found));
     }
 }
