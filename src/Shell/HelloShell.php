c3c082498aa1a3e31ebf2437aea912ee559398bd
MVIN <matthias.vincent@manessens.com>
2018-08-08 14:32:27 +0200

    Update HelloShell.php

--- src/Shell/HelloShell.php
+++ src/Shell/HelloShell.php
@@ -5,8 +5,24 @@ use Cake\Console\Shell;
 
 class HelloShell extends Shell
 {
+
+    public function initialize()
+    {
+        parent::initialize();
+        $this->loadModel('Users');
+    }
+
     public function main()
     {
         $this->out('Hello world.');
+    }
+
+    public function show(){
+        if (empty($this->args[0])) {
+            // Utilisez error() avant CakePHP 3.2
+            return $this->abort("Merci de rentrer un nom d'utilisateur.");
+        }
+        $user = $this->Users->findByEmail($this->args[0])->first();
+        $this->out(print_r($user, true));
     }
 }
