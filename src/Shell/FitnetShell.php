9cde97e716778c41aafc36daf39f6d19868c6716
MVIN <matthias.vincent@manessens.com>
2018-08-08 15:21:12 +0200

    Update FitnetShell.php

--- src/Shell/FitnetShell.php
+++ src/Shell/FitnetShell.php
@@ -2,6 +2,7 @@
 namespace App\Shell;
 
 use Cake\Console\Shell;
+use App\Controller\ProjetController;
 
 class FitnetShell extends Shell
 {
@@ -19,42 +20,47 @@ class FitnetShell extends Shell
         if (empty($this->args[0])) {
             return $this->abort("Merci de rentrer un email d'utilisateur.");
         }
+
+        $projet = new ProjetController();
+
+
         // $user = $this->Users->findByEmail($this->args[0])->first();
         // $this->out(print_r($user, true));
 
         // $this->createFile('report.json', json_encode($user));
+
         // ***************************************************************
-        $found = [];
-
-        // récupération des id company fitnet
-        $client = $this->Client->findByIdc($id_client, [
-            'contain' => ['Agence']
-        ]);
-
-        $id_fit = $client->agence->id_fit;
-
-        // séparation des id_agence fitnet
-        $ids = explode(';', $id_fit);
-
-        // appel de la requête
-        $result = $this->getFitnetLink("/FitnetManager/rest/projects/".$this->args[0]);
-        // décode du résultat json
-        $vars = json_decode($result, true);
-        // sauvegarde des résultats trouvés
-        $found = array_merge($found, $vars);
-
-        $select2 = ['select' => array(), 'projects' => array()];
-        //remise en forme du tableau
-        foreach ($found as $value) {
-            if ($value['customer'] == $client->id_fit or $client->id_fit == null) {
-                $select2['select'][]=array('id'=>$value['forfaitId'], 'text'=>$value['title']);
-                $select2['projects'][$value['forfaitId']]=$value;
-            }
-        }
-
-        // réencodage pour renvoie au script ajax
-        $json_found = json_encode($select2);
-
-        $this->createFile('report.json', json_encode($json_found));
+        // $found = [];
+        //
+        // // récupération des id company fitnet
+        // $client = $this->Client->findByIdc($id_client, [
+        //     'contain' => ['Agence']
+        // ]);
+        //
+        // $id_fit = $client->agence->id_fit;
+        //
+        // // séparation des id_agence fitnet
+        // $ids = explode(';', $id_fit);
+        //
+        // // appel de la requête
+        // $result = $this->getFitnetLink("/FitnetManager/rest/projects/".$this->args[0]);
+        // // décode du résultat json
+        // $vars = json_decode($result, true);
+        // // sauvegarde des résultats trouvés
+        // $found = array_merge($found, $vars);
+        //
+        // $select2 = ['select' => array(), 'projects' => array()];
+        // //remise en forme du tableau
+        // foreach ($found as $value) {
+        //     if ($value['customer'] == $client->id_fit or $client->id_fit == null) {
+        //         $select2['select'][]=array('id'=>$value['forfaitId'], 'text'=>$value['title']);
+        //         $select2['projects'][$value['forfaitId']]=$value;
+        //     }
+        // }
+        //
+        // // réencodage pour renvoie au script ajax
+        // $json_found = json_encode($select2);
+        //
+        // $this->createFile('report.json', json_encode($json_found));
     }
 }
