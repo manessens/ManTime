c51a997e1a813720ffddf10a2c023a4ea7695172
MVIN <matthias.vincent@manessens.com>
2018-08-20 14:59:34 +0200

    Update ExportFitnetController.php

--- src/Controller/ExportFitnetController.php
+++ src/Controller/ExportFitnetController.php
@@ -402,7 +402,7 @@ class ExportFitnetController extends AppController
                     if ($assignement['employeeID'] == $time->user->id_fit
                     && $assignement['customerID'] == $time->client->id_fit
                     && $assignement['projectID'] == $time->projet->id_fit) {
-                        insertLog(null, 'assignement found');
+                        $this->insertLog(null, 'assignement found');
 
                         return $assignement[$assignementIdName[$activityType]];
                     }
@@ -416,7 +416,7 @@ class ExportFitnetController extends AppController
                     && $assignement['customerID'] == $time->client->id_fit
                     && $assignement['projectID'] == $time->projet->id_fit
                     && $date_debut <= $time->date && $date_fin >= $time->date ) {
-                        insertLog(null, 'assignement found');
+                        $this->insertLog(null, 'assignement found');
 
                         return $assignement[$assignementIdName[$activityType]];
                     }
