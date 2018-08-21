c695bcee20af10192805eb6de88ff22559b6387f
MVIN <matthias.vincent@manessens.com>
2018-08-21 11:24:22 +0200

    Update ExportFitnetController.php

--- src/Controller/ExportFitnetController.php
+++ src/Controller/ExportFitnetController.php
@@ -102,7 +102,7 @@ class ExportFitnetController extends AppController
 
         $filename = Configure::read('fitnet.logname_end') . $id . '.csv';
 
-        $folder = new Folder('/fitnet_log/final');
+        $folder = new Folder(Configure::read('fitnet.abs_path').Configure::read('fitnet.logdir_end'));
         $file = $folder->find($filename);
         pr($folder);
         pr($file);exit;
