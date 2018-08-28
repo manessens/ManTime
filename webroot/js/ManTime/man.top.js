bcd445b091d8f2a9b30f0d21e25dffbeaf441312
MVIN <matthias.vincent@manessens.com>
2018-08-28 14:04:16 +0200

    Update man.top.js

--- webroot/js/ManTime/man.top.js
+++ webroot/js/ManTime/man.top.js
@@ -21,6 +21,14 @@ function scrollFunction() {
 function initWeeker(){
     $('#select-week').show();
     $('#select-week').on('change', function(e){
-        
+        var weeker = $(this).val();
+        var annee = weeker.substring(0, 4);
+        var week = weeker.substring(7, 8);
+        document.location.replace('/temps/index/'+week+'/'+annee);
+        // console.log('/temps/index/'+twoDigit(week)+'/'+annee);
     })
+}
+
+function twoDigit(n) {
+    return (n < 10 ? '0' : '') + n
 }
