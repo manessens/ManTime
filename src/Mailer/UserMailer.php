83d22869ba37ac0da6cf98ff4cbc3bb9aad03536
MVIN <matthias.vincent@manessens.com>
2018-08-30 12:21:44 +0200

    Update UserMailer.php

--- src/Mailer/UserMailer.php
+++ src/Mailer/UserMailer.php
@@ -8,7 +8,7 @@ class UserMailer extends Mailer
     public function welcome($user)
     {
         // $this->to($user->email)
-        $this->to('matthias.vincent@manessens.com')
+        $this->to($user->email)
             ->subject(sprintf('Bienvenu %s sur ManTime', $user->fullname))
             ->viewVars(['fullname'=>$user->fullname])
             ->emailFormat('html');
@@ -16,8 +16,7 @@ class UserMailer extends Mailer
 
     public function resetPassword($user)
     {
-        $this
-            ->to($user->email)
+        $this->to($user->email)
             ->subject('Réinitialisation du mot de passe ManTime')
             ->viewVars(['fullname'=>$user->fullname])
             ->emailFormat('html');
