dad1fc8efc725730fd0061a65e51e6923377dc1b
MVIN <matthias.vincent@manessens.com>
2018-09-27 11:45:00 +0200

    Update ExportFitnetController.php

--- src/Controller/ExportFitnetController.php
+++ src/Controller/ExportFitnetController.php
@@ -512,19 +512,15 @@ class ExportFitnetController extends AppController
         foreach ($assignementTable as $assignement) {
             $date_debut = new Time(str_replace('/', '-', $assignement['assignmentStartDate']));
             $date_fin = new Time(str_replace('/', '-', $assignement['assignmentEndDate']));
-            debug($time->user->id_fit);
-            debug($time->projet->client->id_fit);
-            debug($time->projet->id_fit);
-            debug($time->date);
             if ($assignement['employeeID'] == $time->user->id_fit
             && $assignement['customerID'] == $time->projet->client->id_fit
             && $assignement['contractID'] == $time->projet->id_fit
             // && $date_debut <= $time->date && $date_fin >= $time->date
             ) {
                 debug($assignement);
+                exit;
                 return $assignement[$assignementIdName[$activityType]];
             }
-            exit;
         }
         return;
 
