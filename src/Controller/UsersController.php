3db76ee48f55af3c72ec1245d6a4173301c3ea4e
MVIN <matthias.vincent@manessens.com>
2019-02-25 11:15:47 +0100

    other debug

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -270,6 +270,7 @@ class UsersController extends AppController
                 $result = $this->getFitnetLink("/FitnetManager/rest/employees");
                 // décode du résultat json
                 $vars = json_decode($result, true);
+                debug($vars)
                 if (is_array($vars)) {
                     $key_found = array_search($mail, array_column($vars, 'email'));
 
