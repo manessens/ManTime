1e1271bebd240d11a38e43f7b8b53afd1ea7f1c6
MVIN <matthias.vincent@manessens.com>
2020-06-19 09:11:10 +0200

    remove debug

--- src/Controller/MatriceController.php
+++ src/Controller/MatriceController.php
@@ -71,7 +71,7 @@ class MatriceController extends AppController
                     $line->idm = $matrice->idm;
                 }
                     // DEBUG:
-                    debug($matrice->lign_mat); exit;
+                    // debug($matrice->lign_mat); exit;
                 // $matrice->lign_mat = $lignMats;
                 // $matrice->lign_mat[0]->idm = $matrice->idm;
                 // $matrice->lign_mat[0]->id_profil = 1;
