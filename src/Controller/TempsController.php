043150e7b20f3ce31bb6ff1e747510e96968f26a
MIN <mvincent@synvance.com>
2020-09-24 11:05:54 +0200

    Update TempsController.php

--- src/Controller/TempsController.php
+++ src/Controller/TempsController.php
@@ -727,19 +727,17 @@ class TempsController extends AppController
 
             if ($verif) {
                 // $this->Flash->success(__('La semaine à été sauvegardée.'));
-                // if (array_key_exists('check_lock', $arrayData)) {
-                //     return $this->redirect(['action' => 'index-admin', $semaine, $annee]);
-                // }
+                if (array_key_exists('check_lock', $arrayData)) {
+                    return $this->redirect(['action' => 'index-admin', $semaine, $annee]);
+                }
                 $string = "success";
             } else {
                 // $this->Flash->error(__('Une erreur est survenue, veuilez contrôler votre saisie avant de réessayer.'));
                 $string = "error";
             }
-            // DEBUG:
-            debug(array_key_exists('check_lock', $arrayData));
-            exit;
+            
             // on ne génère pas la page si on viens d'un appel JS
-            if( $this->request->is('ajax') ) {
+            if( !array_key_exists('check_lock', $arrayData) ) {
                 $this->autoRender = false; // Pas de rendu
                 return $this->response->withStringBody($string);
             }
